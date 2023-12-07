62064a6c00a2f7aee885e8fc9824933551cd293c
Evemose <4upa4undra@gmail.com>
2023-12-07 14:09:26 +0200

    init

---
+++ src/main/java/org/mathhelper/MathhelperApplication.java
@@ -0,0 +1,13 @@
+package org.mathhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathhelperApplication.class, args);
+    }
+
+}
